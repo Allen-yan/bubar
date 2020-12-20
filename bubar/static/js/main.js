3f4ac5ec666d89ff5cc9171499e8dc73e4a6053c
Cook Yan <wentao.yan@andui.com>
2020-12-20 21:19:53 +0800

    页面信息同步

--- bubar/static/js/main.js
+++ bubar/static/js/main.js
@@ -17,10 +17,24 @@ $(document).ready(function() {
           var new_text=$(this).val();
           $(this).remove();
           td_dom.text(new_text);
-
-
        });
     });
 
+    function focusoutHandler(params){
+        var val = $(this).find("input").val();
+        for (var k in params.data ){
+//            alert(k+" "+params.data[k]);
+            $("#"+params.data[k]).text(val);
+        }
+    }
 
+    $("#c_7_12").on("focusout",{ele_id: "c_39_12"}, focusoutHandler) ;
+    $("#c_10_9").on("focusout",{ele_id: "c_42_12"}, focusoutHandler) ;
+    $("#c_11_9").on("focusout",{ele_id: "c_41_12"}, focusoutHandler) ;
+    $("#c_13_7").on("focusout",{ele_id: "c_32_12"}, focusoutHandler) ;
+    $("#c_13_14").on("focusout",{ele_id1: "c_32_9", ele_id2: "c_45_12"}, focusoutHandler) ;
+    $("#c_17_9").on("focusout",{ele_id: "c_40_12"}, focusoutHandler) ;
+    $("#c_18_9").on("focusout",{ele_id: "c_43_12"}, focusoutHandler) ;
+    $("#c_18_12").on("focusout",{ele_id: "c_44_12"}, focusoutHandler) ;
+    $("#c_22_9").on("focusout",{ele_id: "c_25_12"}, focusoutHandler) ;
 });
